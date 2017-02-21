7d24fa39847ad7a29a7c333948763f8a9c82a213
Elisa M Strope <stropeem@lopers.unk.edu>
2017-02-21 11:43:30 -0600

    Initial commit

---
+++ src/lab5_stropeem/Lab5.java
@@ -0,0 +1,52 @@
+package lab5_stropeem;
+
+/**
+ * Created by stropeem on 2/21/2017.
+ */
+public class Lab5 {
+
+    /**
+     * return true if the string is a palindrome
+     * A recursive method
+     * @param string
+     * @return
+     */
+    public static void palindrome(String string){
+        boolean value = false;
+
+        if()//string is a palindrom)
+            value=true;
+    }
+
+    /**
+     * same thing as palindrome() but it works iteratively
+     * @param string
+     * @return
+     */
+    public static void palindromeIterative(String string){
+        int count = 0;
+
+        if()//string is a palindrome
+            count++;
+
+    }
+
+    /**
+     * read the text as a string
+     * callss palindrome() and/or palindromeIterative()
+     */
+    public static void palindromeTest(){
+       //call the text as a string from the file
+       palindrome("yes");
+       palindromeIterative("yes");
+    }
+
+    public static void main(String[] args){
+
+    }
+    /**
+     * n*fact(n-1) if n>1
+     * if(n=1)
+     * n<=0
+     */
+}
